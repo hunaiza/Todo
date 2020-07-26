0bf296655d7e59f2a0b9683a23770e2e1b4ab7d8
hunaiza <address>
2020-07-27 04:40:30 +0500

    Todo

---
+++ public/app2.js
@@ -0,0 +1,47 @@
+var list = document.getElementById("list")
+
+function addTodo (){
+    var todo_item = document.getElementById("todo-item")
+    
+    //Create li tag with code
+    var li = document.createElement('li');
+    li.setAttribute("id","li");
+    var liText = document.createTextNode(todo_item.value)
+    var input = document.createElement("input");
+    input.setAttribute("type","text");
+    input.setAttribute("id","edit")
+    li.appendChild(input)
+    li.appendChild(liText);
+
+    //Create Delete button
+    var delbtn = document.createElement("button");
+    delbtn.setAttribute("class","btn");
+    delbtn.setAttribute("onclick","deleteItem(this)");
+
+    //Create Edit button 
+    var editBtn = document.createElement("button");
+    editBtn.setAttribute("class","btn1");
+    editBtn.setAttribute("onclick","editItem(this)");
+
+
+
+    li.appendChild(delbtn)
+    li.appendChild(editBtn)
+
+    list.appendChild(li);
+    todo_item.value = "";
+    console.log(li);
+}
+
+function deleteItem(e){
+    e.parentNode.remove()
+}
+
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+    var editValue = prompt("Enter?",val)
+    e.parentNode.firstChild.nodeValue = editValue
+}
+function refresh1(){
+    window.location.reload(false); 
+}
